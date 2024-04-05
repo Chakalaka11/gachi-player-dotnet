da32c756bbdcf3ac5758e67d4d78a57410cb84be
Domin Viktor <dyomin.vic@gmail.com>
2024-04-05 23:53:46 +0300

    Added lavalink

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Lavalink;
+using DSharpPlus.Net;
 using DSharpPlus.SlashCommands;
 using Microsoft.Extensions.Configuration;
 
@@ -20,10 +22,26 @@ class Program
             TokenType = TokenType.Bot,
             Intents = DiscordIntents.Guilds | DiscordIntents.GuildVoiceStates
         });
+        var endpoint = new ConnectionEndpoint
+        {
+            Hostname = "127.0.0.1", // From your server configuration.
+            Port = 2333 // From your server configuration
+        };
+
+        var lavalinkConfig = new LavalinkConfiguration
+        {
+            Password = configuration["LavalinkPass"], // From your server configuration.
+            RestEndpoint = endpoint,
+            SocketEndpoint = endpoint
+        };
+
+        var lavalink = discord.UseLavalink();
         var slash = discord.UseSlashCommands();
         slash.RegisterCommands<SlashCommands>();
+        slash.RegisterCommands<PlayerCommands>();
 
         await discord.ConnectAsync();
+        await lavalink.ConnectAsync(lavalinkConfig); 
         await Task.Delay(-1);
     }
 }
--- SlashCommands.cs
+++ SlashCommands.cs
@@ -1,15 +1,12 @@
-using DSharpPlus;
-using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 
 namespace GachiPlayerDotnet;
+
 public class SlashCommands : ApplicationCommandModule
 {
     [SlashCommand("ping", "To verify if everything is working.")]
-    public async Task DelayTestCommand(InteractionContext ctx)
+    public async Task PingCommand(InteractionContext ctx)
     {
-        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-
-        await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Oh my shoulder! v3"));
+        await ctx.CreateResponseAsync("Oh my shoulder! v3");
     }
 }
